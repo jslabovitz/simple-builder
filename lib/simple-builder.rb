f51c29101f0e55c24efcc6f377c4a6b4a9028a7f
John Labovitz <johnl@johnlabovitz.com>
2022-11-21 02:29:15 +0100

    Combine several files into one.

--- lib/simple-builder.rb
+++ lib/simple-builder.rb
@@ -1,8 +1,23 @@
 require 'nokogiri'
 
+require 'simple-builder/fragments'
+require 'simple-builder/helpers'
+
 module Simple
 
   class Builder
+
+    def self.html4_document(&block)
+      HTML4Document.build(&block)
+    end
+
+    def self.html5_document(&block)
+      HTML5Document.build(&block)
+    end
+
+    def self.html_fragment(&block)
+      HTMLFragment.build(&block)
+    end
 
     def self.build(&block)
       new.build(&block)
@@ -15,13 +30,47 @@ module Simple
       @root
     end
 
+    class HTMLDocument < Builder
+
+      def initialize
+        super
+        @root = Nokogiri::HTML::Document.new
+        @root.encoding = 'UTF-8'
+        @root.internal_subset.remove
+        @root.create_internal_subset(*internal_subset) if internal_subset
+      end
+
+      def internal_subset
+        nil
+      end
+
+    end
+
+    class HTML4Document < HTMLDocument
+
+      def internal_subset
+        ['html', '-//W3C//DTD HTML 4.01 Transitional//EN', 'http://www.w3.org/TR/html4/loose.dtd']
+      end
+
+    end
+
+    class HTML5Document < HTMLDocument
+
+      def internal_subset
+        ['html', nil, nil]
+      end
+
+    end
+
+    class HTMLFragment < Builder
+
+      def initialize
+        super
+        @root = Nokogiri::HTML.fragment('')
+      end
+
+    end
+
   end
 
 end
-
-require 'simple-builder/fragments'
-require 'simple-builder/helpers'
-require 'simple-builder/html_document'
-require 'simple-builder/html4_document'
-require 'simple-builder/html5_document'
-require 'simple-builder/html_fragment'
