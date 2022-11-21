11541ac4f1fa1897a4791580f44129838ef2b9b2
John Labovitz <johnl@johnlabovitz.com>
2022-11-21 12:30:34 +0100

    Remove Google Analytics bug.

--- lib/simple-builder/fragments.rb
+++ lib/simple-builder/fragments.rb
@@ -4,25 +4,6 @@ module Simple
 
     class << self
 
-      def google_analytics(tracker_id)
-        html_fragment do |html|
-          html.script(type: 'text/javascript') do
-            html << %Q{
-              var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-              document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-            }
-          end
-          html.script(type: 'text/javascript') do
-            html << %Q{
-              try {
-                var pageTracker = _gat._getTracker("#{tracker_id}");
-                pageTracker._trackPageview();
-              } catch(err) {}
-            }
-          end
-        end
-      end
-
       def link_if(state, &block)
         html_fragment do |html|
           elem = html_fragment { |h| yield(h) }
